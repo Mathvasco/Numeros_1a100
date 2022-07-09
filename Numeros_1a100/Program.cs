91eaf0457482167e13655a9d8e9d7659242ee44f
NoUser <NoUser@PC30K>
2022-07-09 15:08:52 -0300

    Adicionar arquivos de projeto.

---
+++ Numeros_1a100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_1a100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            for (numero = -1; numero < 100; numero++)
+            {
+                numero = numero + 1;
+                Console.WriteLine(numero);
+            }
+            Console.ReadKey();
+        }
+    }
+}
